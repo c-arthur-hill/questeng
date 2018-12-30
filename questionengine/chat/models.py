1aee4494fffa6cd58005b40b7714117a764387a3
Ubuntu <ubuntu@ip-172-31-28-124.us-west-2.compute.internal>
2018-12-30 05:57:01 +0000

    basic ability to save message and UI tweaks

--- questionengine/chat/models.py
+++ questionengine/chat/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Message(models.Model):
+    from_ava = models.BooleanField(default=False)
+    message = models.CharField(max_length=2047)
+    user = models.ForeignKey(User, models.SET_NULL, blank=True, null=True)
+
+    def __str__(self):
+        return message
--- questionengine/chat/views.py
+++ questionengine/chat/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from .forms import MessageForm
+from .models import Message
 
 def home(request):
-    return render(request, 'home.html')
+    if request.method == 'POST':
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            new_message = form.save(commit=False)
+            new_message.save()
+    form = MessageForm()
+    messages = Message.objects.all()
+    return render(request, 'home.html', {'form': form, 'messages': messages})
 
 def about(request):
     return render(request, 'about.html')
