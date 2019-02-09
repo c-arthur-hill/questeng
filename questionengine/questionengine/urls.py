d225f62d28bf5fd846a2d1dbd4e0a4965d868444
Calvin <c.arthur.hill@gmail.com>
2019-02-09 02:03:02 +0000

    Redid basically the entire app. Styling. Created article model. Created basic forms for creating article

--- questionengine/questionengine/urls.py
+++ questionengine/questionengine/urls.py
@@ -16,16 +16,20 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path
-from articles.views import about, home
+from articles.views import about, home, create_article, choose_article_part_type, create_paragraph, create_subheader, create_topic
 from accounts.views import register, login
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('register/', register, name='register'),
-    path('register/<int:conversation_id>', register, name='register_id'),
-    path('login/', login, name='login'),
-    path('login/<int:conversation_id>/', login, name='login_id'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('admin', admin.site.urls),
+    path('register', register, name='register'),
+    path('login', login, name='login'),
+    path('logout', auth_views.LogoutView.as_view(), name='logout'),
     path('', home, name='home'),
-    path('about', about, name='about'),    
+    path('about', about, name='about'),
+    path('topic/create', create_topic, name='create_topic'),
+    path('article/create', create_article, name='create_article'),
+    path('article/<int:article_id>/topic/create', create_topic, name='create_topic_id'),
+    path('article/<int:article_id>/subheader/create', create_subheader, name='create_subheader'),
+    path('article/<int:article_id>/paragraph/create', create_paragraph, name='create_paragraph'),
+    path('article/<int:article_id>/part_type/choose', choose_article_part_type, name='choose_article_part_type'),
 ]
